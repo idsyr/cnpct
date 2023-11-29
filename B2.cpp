81c7f9c25294d274a309518e9c266fb185e5191c
idsyr <idsyr@mail.ru>
2023-11-29 18:33:40 +0500

    B2

---
+++ B2.cpp
@@ -0,0 +1,46 @@
+0100
+семантическое значение
+value type - диапазон значений
+имя
+object type - совокупность операций
+статическая типизация - тип связан с именем
+адрес
+CHAR_BIT char минимально адресуемая ячейка
+
+int x, y;
+int &xref = x;
+xref = y;
+int *xptr = &xref;
+int &*xrefptr = &xref;//!
+int *&xptrref = xptr;
+
+int x;
+int &y = x;
+int[2] = {10, 20};
+int &xref = x[0];
+int* xptr = &x[0];
+xref += 1;
+xptr += 1;
+assert(xref  == 11);
+assert(*xptr == 20);
+
+const char* &s1;
+char const* $s2;
+char* const s3;
+cahr const * const s4;
+char &r1 = r;
+const char &r2 = r1;
+
+
+int foo(){return 42;}
+const int &l = foo();
+c-инкапсуляция: обьект на стеке, проблемы с инлайном
+инкапсуляция - сохранение инварианта класса
+модификатор на имена данных
+консистентное состояние
+ссылка - инкапсуляция указателя
+инкапусуляция это свойство типа
+
+
+
+
