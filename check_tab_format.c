7c2b75cdb61ed62d642385b7fa6f2b4b586bc63a
idsyr <idsyr@mail.ru>
2024-08-04 00:58:34 +0500

    Check space format

--- check_tab_format.c
+++ check_tab_format.c
@@ -3,9 +3,9 @@ void copy(int *to, int const *from, int count){
 	int n = (count + 3) / 4;
 	switch(count%4){
 		case 0: do { *to++ = *from++;
-		case 3:			 *to++ = *from++;
-		case 2:			 *to++ = *from++;
-		case 1:			 *to++ = *from++;
+		case 3:	     *to++ = *from++;
+		case 2:      *to++ = *from++;
+		case 1:      *to++ = *from++;
 						} while (--n > 0);
 	}
 }
