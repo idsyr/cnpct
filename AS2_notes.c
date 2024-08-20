e43f106e82686919cfd069ec1e8c4b6ceaaafea5
idsyr <idsyr@mail.ru>
2024-08-20 21:03:14 +0500

    modified: (workable state)AS2_notes.c

--- AS2_notes.c
+++ AS2_notes.c
@@ -23,19 +23,19 @@ typedef enum { MOVI = 0x0, ADD = 0x8, SUB = 0x9,
 typedef enum {reg_load_format, ariphm_format, screen_format} instr_format_t;
 
 typedef struct {
-	uint8_t opcode  :1;
 	uint8_t immediat :7;
+	uint8_t opcode  :1;
 } instr_format_reg_load_t;
 
 typedef struct {
-	uint8_t opcode  :4;
-	registr_t reg_1 :2;
 	registr_t reg_2 :2;
+	registr_t reg_1 :2;
+	uint8_t opcode  :4;
 } instr_format_ariphm_t;
 
 typedef struct {
-	uint8_t opcode  :6;
 	uint8_t reg_1   :2;
+	uint8_t opcode  :6;
 } instr_format_screen_t;
 
 
@@ -51,7 +51,7 @@ typedef struct {
 
 
 void print_movi_asm(uint8_t *hexcode){
-	instr_format_reg_load_t *movi = hexcode;
+	instr_format_reg_load_t const *movi = (instr_format_reg_load_t*) hexcode;
 	printf("MOVI %d", movi->immediat);
 	
 }
@@ -66,7 +66,7 @@ instr_info_t movi_info = {
 
 
 void print_add_asm(uint8_t *hexcode){
-	instr_format_ariphm_t *add = hexcode;
+	instr_format_ariphm_t const *add = (instr_format_ariphm_t*) hexcode;
 	printf("ADD %c %c", add->reg_1 + 65, add->reg_2 + 65);
 }
 
@@ -80,8 +80,8 @@ instr_info_t add_info = {
 
 
 void print_sub_asm(uint8_t *hexcode){
-	instr_format_ariphm_t *sub = hexcode;
-	printf("SUB %c %c", sub->reg_1 + 65, sub->reg_2 + 65;
+	instr_format_ariphm_t const *sub = (instr_format_ariphm_t*) hexcode;
+	printf("SUB %c %c", sub->reg_1 + 65, sub->reg_2 + 65);
 }
 
 const char pattern_hex_sub[] = "0x9.";
@@ -94,8 +94,8 @@ instr_info_t sub_info = {
 
 
 void print_mul_asm(uint8_t *hexcode){
-	instr_format_ariphm_t *mul = hexcode;
-	printf("MUL %c %c", mul->reg_1 + 65, mul->reg_2 + 65;
+	instr_format_ariphm_t const *mul = (instr_format_ariphm_t*) hexcode;
+	printf("MUL %c %c", mul->reg_1 + 65, mul->reg_2 + 65);
 }
 
 const char pattern_hex_mul[] = "0xA.";
@@ -108,8 +108,8 @@ instr_info_t mul_info = {
 
 
 void print_div_asm(uint8_t *hexcode){
-	instr_format_ariphm_t *div = hexcode;
-	printf("DIV %c %c", div->reg_1 + 65, div->reg_2 + 65;
+	instr_format_ariphm_t const *div = (instr_format_ariphm_t*) hexcode;
+	printf("DIV %c %c", div->reg_1 + 65, div->reg_2 + 65);
 }
 
 const char pattern_hex_div[] = "0xB.";
@@ -122,8 +122,8 @@ instr_info_t div_info = {
  
 
 void print_in_asm(uint8_t *hexcode){
-	instr_format_screen_t *in = hexcode;
-	printf("IN %c", in->reg_1);
+	instr_format_screen_t const *in = (instr_format_screen_t*) hexcode;
+	printf("IN %c", in->reg_1 + 65);
 }
 
 const char pattern_hex_in[] = "0xC[0-3]";
@@ -136,8 +136,8 @@ instr_info_t in_info = {
 
 
 void print_out_asm(uint8_t *hexcode){
-	instr_format_screen_t *out = hexcode;
-	printf("OUT %c", out->reg_1);
+	instr_format_screen_t const *out = (instr_format_screen_t*) hexcode;
+	printf("OUT %c", out->reg_1 + 65);
 }
 
 const char pattern_hex_out[] = "0xC[4-7]";
@@ -170,39 +170,28 @@ void check_regcompexec(int res, regex_t* regex_arr, int i){
 
 
 
-void read_hex_instruction_init(regex_t *regex_arr, instr_info_t *instr_set){
+void decode_hex_instruction_init(regex_t *regex_arr, instr_info_t *instr_set){
 	for(int i = 0; i<instruction_set_n;){
-		int res = regcomp(&regex_arr[i], instr_set[i].asm_pattern, REG_EXTENDED);
+		int res = regcomp(regex_arr + i, instr_set[i].hex_pattern, REG_EXTENDED);
 		check_regcompexec(res, regex_arr, ++i);
 	}	
 }
 
 
 
-instruction_t read_hex_instruction(regex_t *regex_arr, instr_info_t *instr_set){
-	instruction_t instr;	instr.info = NULL;
-	char instr_asm[5];
+instruction_t decode_hex_instruction(regex_t *regex_arr, instr_info_t *instr_set, char instr_hex[5]){
+	instruction_t instr;	instr.info = NULL; instr.val = 0;
 	regmatch_t matches[1];
-
-	int res = scanf("%4s", instr_asm);
-	if(res!=1) return instr;
-
+	
 	for(int i = 0; i<instruction_set_n; ++i){
-		res = regexec(regex_arr + i, instr_asm, 1, matches, 0);
+		int res = regexec(regex_arr + i, instr_hex, 1, matches, 0);
 		check_regcompexec(res, regex_arr, instruction_set_n);
 		if(!res){
 			instr.info = instr_set + i;
-			
-			char buff[2] = "";
-			memcpy(buff,
-						 instr_asm + matches[0].rm_so,
-						 matches[i].rm_eo - matches[i].rm_so);
-
-			instr.val = strtol(buff, NULL, 16);
+			instr.val = strtol(instr_hex, NULL, 16);
 			return instr;
 		}
 	}
-	
 	return instr;
 }
 
@@ -211,21 +200,35 @@ int main(){
 		{movi_info, add_info, sub_info, mul_info, div_info, in_info, out_info};
 	instruction_set_n = sizeof(instruction_set) / sizeof(instruction_set[0]);
 	regex_t instruction_regex[instruction_set_n];
-	read_hex_instruction_init(instruction_regex, instruction_set);
+	
+	decode_hex_instruction_init(instruction_regex, instruction_set);
 	for(;;){
-		instruction_t i = read_hex_instruction(instruction_regex, instruction_set);
-		if(i.info == NULL) break;
+		char instr_hex[5];
+		int res = scanf("%4s", instr_hex);
+		if(res != 1) break;
+		instruction_t i = decode_hex_instruction(instruction_regex, instruction_set, instr_hex);
+		if(i.info == NULL) {fprintf(stderr, "Unrecognized instr\n"); continue;}
 		i.info->print_as_asm(&i.val);
+		printf("\n");
 	}
 }
 
 
 /*
-| MOVI | I   | 0IIIIIII |
-| ADD  | R,R | 1000RRRR |
-| SUB  | R,R | 1001RRRR |
-| MUL  | R,R | 1010RRRR |
-| DIV  | R,R | 1011RRRR |
-| IN   | R   | 110000RR |
-| OUT  | R   | 110001RR |
+[ids@archlinux cnpct]$ cat 001.dat
+0x70 0xC7 0xC1 0x87 0x27 0xC5 0x8D 0xC1 0x87 0x6f 0xC5 0xC7
+[ids@archlinux cnpct]$ ./AS2_notes.e < 001.dat
+MOVI 112
+OUT D
+IN B
+ADD B D
+MOVI 39
+OUT B
+ADD D B
+IN B
+ADD B D
+MOVI 111
+OUT B
+OUT D
+[ids@archlinux cnpct]$
 */
